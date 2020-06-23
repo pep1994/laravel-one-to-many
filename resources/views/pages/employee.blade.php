55a50661af9350ccc5f8c7db1a8fa920ad06f8fc
Marco Petrini <marcopetrini1994@gmail.com>
2020-06-23 17:46:34 +0200

    print tasks

--- resources/views/pages/employee.blade.php
+++ resources/views/pages/employee.blade.php
@@ -15,6 +15,16 @@
                             {{ $location -> street }} - {{ $location ->  city}} - {{ $location -> state }}
                         </li>
                     @endforeach
+                </ul> <br>
+                <h5>Tasks: </h5>
+                <ul>
+                    @foreach ($employee -> tasks as $task)
+                        <li>
+                            <h6>{{ $task -> name}}</h6>
+                            <p>{{ $task -> description}}</p>
+                            <p>{{ $task -> deadline}}</p>           
+                        </li>
+                    @endforeach
                 </ul>
                 <a type="button" class="btn btn-success" href="{{ route('edit_employee', $employee['id']) }}">Edit</a>
                 <a type="button" class="btn btn-danger" href="{{ route('delete_employee', $employee['id']) }}">Delete</a>
